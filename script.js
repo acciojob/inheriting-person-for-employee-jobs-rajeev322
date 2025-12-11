a23f015ca243531ebe713b22b45446b7d7541df3
RAJEEV CHOWDARY <150281256+rajeev322@users.noreply.github.com>
2025-12-11 20:30:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // complete this js code
 function Person(name, age) {
-	 this.name = name;
+  this.name = name;
   this.age = age;
 }
+
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
+
 function Employee(name, age, jobTitle) {
-	  Person.call(this, name, age); // inherit Person properties
+  Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
-Employee.prototype = Object.create(Person.prototype); // inherit methods
+
+Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
